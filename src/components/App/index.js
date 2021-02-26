8e9749e0ea68ae7ab3be9c9bf1996353b448603c
joshgoshbgosh <69131589+joshgoshbgosh@users.noreply.github.com>
2021-02-25 22:45:38 -0500

    update

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -23,12 +23,12 @@ class App extends Component {
       <Menu/>
         <div className="App">
           <Switch>
-            <Route path='str8jacketphotography/'component={Home}  exact />
-            <Route path='str8jacketphotography/about'component={About}/>
-            <Route path='str8jacketphotography/contact'component={Contact}/>
-            <Route path='str8jacketphotography/gallery'component={Gallery}/>
-            <Route path='str8jacketphotography/photography'component={Photography}/>
-            <Route path='str8jacketphotography/videography'component={Videography}/>
+            <Route path='/'component={Home}  exact />
+            <Route path='/about'component={About}/>
+            <Route path='/contact'component={Contact}/>
+            <Route path='/gallery'component={Gallery}/>
+            <Route path='/photography'component={Photography}/>
+            <Route path='/videography'component={Videography}/>
           </Switch>
         </div>
       <Footer/>
