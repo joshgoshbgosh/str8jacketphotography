260d3036e054fb41ffc5bd5103385efbb83ee04c
joshgoshbgosh <69131589+joshgoshbgosh@users.noreply.github.com>
2021-02-27 09:08:09 -0500

    additional update

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -17,17 +17,17 @@ function Header(props){
     <Navbar.Collapse id="responsive-navbar-nav" >
 
         <Nav>
-          <Nav.Link className="nav-link nav-home" href='str8jacketphotography/#/'>HOME</Nav.Link>
-          <Nav.Link className="nav-link nav-gallery" href='str8jacketphotography/#/gallery'>GALLERY</Nav.Link>
+          <Nav.Link className="nav-link nav-home" href='/'>HOME</Nav.Link>
+          <Nav.Link className="nav-link nav-gallery" href='/#/gallery'>GALLERY</Nav.Link>
         </Nav>
         <div className="logo">
-        <a className="d-none d-md-block"href="/#/">
+        <a className="d-none d-md-block"href="/">
         <Image src={str8jacket} rounded />
         </a>
         </div>
         <Nav>
-          <Nav.Link className="nav-link nav-about" href='str8jacketphotography/#/about'>ABOUT</Nav.Link>
-          <Nav.Link className="nav-link nav-contact" href='str8jacketphotography/#/contact'>CONTACT</Nav.Link>
+          <Nav.Link className="nav-link nav-about" href='/#/about'>ABOUT</Nav.Link>
+          <Nav.Link className="nav-link nav-contact" href='/#/contact'>CONTACT</Nav.Link>
         </Nav>
 
 
