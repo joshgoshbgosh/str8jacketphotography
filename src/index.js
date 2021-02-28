7d8ed181fd3a282e21a55ace36ff833bafe5c8a1
joshgoshbgosh <69131589+joshgoshbgosh@users.noreply.github.com>
2021-02-27 19:46:21 -0500

    indexjs update

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { HashRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-  <Router>
+  <Router basename="/str8jacketphotography">
     <App />
   </Router>
   </React.StrictMode>,
